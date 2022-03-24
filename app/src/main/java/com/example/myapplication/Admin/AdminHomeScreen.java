8ebd6050b0445300376f9791de059356eff4adb8
Abdullah-Zakhoi <tr.abdullahzakhoi@gmail.com>
2022-03-24 20:08:06 +0300

    added: updated status in AdminHomeScreen to firebase.

--- app/src/main/java/com/example/myapplication/Admin/AdminHomeScreen.java
+++ app/src/main/java/com/example/myapplication/Admin/AdminHomeScreen.java
@@ -1,11 +1,14 @@
 package com.example.myapplication.Admin;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -17,6 +20,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.myapplication.Model.OrderModel;
 import com.example.myapplication.R;
 import com.example.myapplication.login;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -28,18 +33,28 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-public class AdminHomeScreen extends AppCompatActivity {
+public class AdminHomeScreen extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     ListView listView;
     ArrayAdapter<String> ordersListAdapter;
     ProgressDialog progressDialog;
     Button LogOutButtonAdmin;
 
-    ArrayList<OrderModel> ordersList;
+//    ArrayList<OrderModel> ordersList;
     ArrayList<String> orderDetails = new ArrayList<String>();
+    ArrayList<String> orderNumberList = new ArrayList<String>();
+    ArrayList<String> statusList = new ArrayList<String>();
+
+
 
     SharedPreferences sharedpreferences;
     SharedPreferences.Editor editor;
+
+
+    String [] statusListRadioGroup = new String[6] ;
+//    ArrayList<String> statusListRadioGroup = new ArrayList<String>();
+    String radioButton ;
+    int index = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +62,24 @@ public class AdminHomeScreen extends AppCompatActivity {
         setContentView(R.layout.activity_admin_home_screen);
 
         listView = findViewById(R.id.listViewAdmin);
+        listView.setOnItemClickListener(this);
+
+        statusListRadioGroup[0]="قيد الانتضار";
+        statusListRadioGroup[1]="مقبول";
+        statusListRadioGroup[2]="ملغي";
+        statusListRadioGroup[3]="تم استلام الطلب";
+        statusListRadioGroup[4]="تم الشحن";
+        statusListRadioGroup[5]="تم التوصيل";
+
+//        statusListRadioGroup.add( "قيد الانتضار");
+//        statusListRadioGroup.add( "مقبول");
+//        statusListRadioGroup.add( "ملغي");
+//        statusListRadioGroup.add( "تم استلام الطلب");
+//        statusListRadioGroup.add( "تم الشحن");
+//        statusListRadioGroup.add( "تم التوصيل");
+
+        radioButton = statusListRadioGroup[0];
+
 
         LogOutButtonAdmin = findViewById(R.id.LogOutButtonAdmin);
         LogOutButtonAdmin.setOnClickListener(new View.OnClickListener() {
@@ -61,21 +94,19 @@ public class AdminHomeScreen extends AppCompatActivity {
             }
         });
 
+        getOrders();
+
+    }
+
+    void getOrders() {
+
         progressDialog = new ProgressDialog(this, R.style.DialogStyle); //new ProgressDialog(this);
 //        progressDialog.setTitle("Have a nice time");
 //        progressDialog.setMessage(" ");
         progressDialog.setCancelable(true);
         progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
         progressDialog.show();
-//
-        getOrdersOfUser();
 
-
-
-    }
-
-
-    void getOrdersOfUser() {
         final DatabaseReference RootRef;
         RootRef = FirebaseDatabase.getInstance().getReference();
         RootRef.child("AllOrders").addListenerForSingleValueEvent(new ValueEventListener() {
@@ -86,10 +117,9 @@ public class AdminHomeScreen extends AppCompatActivity {
 
                     if (ds.exists()){
 
-                        ordersList =new ArrayList<OrderModel>();
+//                        ordersList =new ArrayList<OrderModel>();
 
                         Map<String,Object> mapObject =(HashMap<String, Object>)ds.getValue();
-
 
                         OrderModel order = new OrderModel();
                         order.setMyEditText1Text(String.valueOf(mapObject.get("myEditText1Text")));
@@ -102,17 +132,24 @@ public class AdminHomeScreen extends AppCompatActivity {
 
                         Log.d("TAG",  String.valueOf( order.getLocationList().get(0)));
 
-                        ordersList.add(order);
+//                        ordersList.add(order);
+
+                        String [] list = new String[]{
+                                "Order Number is : ",
+                                order.getOrderNumber(),
+                                "\nThe order status is : ",
+                                order.getStatus()
+                        };
 
                         orderDetails.add("Order Number is : " + order.getOrderNumber() +  "\nThe order status is : " + order.getStatus() );
-
+                        statusList.add(order.getStatus());
+                        orderNumberList.add(order.getOrderNumber());
                     }
 
                 }
-                Toast.makeText(AdminHomeScreen.this, String.valueOf(orderDetails.size()), Toast.LENGTH_SHORT).show();
+//                Toast.makeText(AdminHomeScreen.this, String.valueOf(orderDetails.size()), Toast.LENGTH_SHORT).show();
 
-                ordersListAdapter = new ArrayAdapter<String>(AdminHomeScreen.this, android.R.layout.simple_list_item_1, orderDetails);
-
+                ordersListAdapter = new ArrayAdapter(AdminHomeScreen.this, android.R.layout.simple_list_item_1, orderDetails);
                 listView.setAdapter(ordersListAdapter);
                 progressDialog.dismiss();
             }
@@ -120,10 +157,95 @@ public class AdminHomeScreen extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 Toast.makeText(AdminHomeScreen.this, String.valueOf(error), Toast.LENGTH_LONG).show();
-
 //                progressBar.setVisibility(View.GONE);
             }
         });
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int itemIndex, long l) {
+        openDialog(itemIndex);
+    }
+
+    void openDialog (int itemIndex){
+
+
+
+//        String current = radioButton;
+
+        for(int i =0;i < statusListRadioGroup.length ; i++){
+            if(statusList.get(itemIndex).equals(statusListRadioGroup[i])){
+                index = i;
+            }
+        }
+        radioButton = statusList.get(itemIndex);
+//         index = statusListRadioGroup.indexOf(statusList.get(itemIndex));
+
+            Toast.makeText(AdminHomeScreen.this, String.valueOf(index) + "  " +radioButton, Toast.LENGTH_SHORT).show();
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("تغير حالة الطلب");
+        builder.setSingleChoiceItems( statusListRadioGroup, index, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+//                radioButton = statusListRadioGroup.get(i);
+                radioButton = statusListRadioGroup[i];
+            }
+        });
+        builder.setPositiveButton("حفظ", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+//             if(!current.equals(radioButton)){
+
+                 statusList.set(itemIndex , radioButton);
+
+                 orderDetails.set(itemIndex , "Order Number is : " + orderNumberList.get(itemIndex) +  "\nThe order status is : " + radioButton);
+
+                 ordersListAdapter.notifyDataSetChanged();
+                updateStatus(orderNumberList.get(itemIndex) , radioButton);
+//             }
+            }
+        });
+
+        builder.setNegativeButton("رجوع", null);
+        builder.show();
+
+    }
+
+    void updateStatus(String orderNumber , String newStatus) {
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+        RootRef.child("AllOrders").child(orderNumber).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+
+                    HashMap<String, Object> orderNumberMap = new HashMap<>();
+                    orderNumberMap.put("status", newStatus);
+
+//                Toast.makeText(AdminHomeScreen.this, orderNumber, Toast.LENGTH_SHORT).show();
+
+                    RootRef.child("AllOrders").child(orderNumber).updateChildren(orderNumberMap)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(AdminHomeScreen.this, "updated status", Toast.LENGTH_SHORT).show();
+                                } else {
+
+                                    Toast.makeText(AdminHomeScreen.this, "Network Error:please try again after some time ...", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
 }
